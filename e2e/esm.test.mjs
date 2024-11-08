7d7265b2484008acbba28e86e78318519b27fb29
Kaffi Y. <gh@109cafe.com>
2024-11-08 17:48:10 +0800

    fix: bump rspack to fix barrel exports (#4)

--- e2e/esm.test.mjs
+++ e2e/esm.test.mjs
@@ -1,6 +1,6 @@
 import { expect, test } from "bun:test";
 
 test("should work with mjs", async () => {
-  const a = import("../dist/label").then((r) => r.default || r);
+  const a = import("../dist/label/exports.mjs").then((r) => r.default || r);
   await expect(a).resolves.toMatchObject({ parse: expect.any(Function) });
 });
